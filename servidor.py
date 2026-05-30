a5f774dddf5ee92671a55a498cef1b5440c0421c
efrener0349 <112995157+efrener0349@users.noreply.github.com>
2026-05-30 00:01:04 -0600

    Create servidor.py

---
+++ servidor.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# === REEMPLAZA CON TU ENLACE DE MONGODB ATLAS ===
+MONGO_URI = "mongodb+srv://efrener0349:jrxwZLg8EtCmrruc@efren.tr0fzs8.mongodb.net/?appName=efren"
+client = MongoClient(MONGO_URI)
+db = client['organizacion_deportiva']
+coleccion_jugadores = db['jugadores']
+
+@app.route('/api/jugadores')
+def obtener_jugadores():
+    jugadores_lista = []
+    for jugador in coleccion_jugadores.find():
+        jugadores_lista.append({
+            "id": str(jugador["_id"]),
+            "nombre": jugador.get("nombre", "Sin Nombre"),
+            "posicion": jugador.get("posicion", "Sin Posición"),
+            "numero": jugador.get("numero", 0)
+        })
+    return jsonify(jugadores_lista)
+
+if __name__ == '__main__':
+    # Render asigna el puerto automáticamente
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
